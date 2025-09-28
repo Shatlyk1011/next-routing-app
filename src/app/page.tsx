e1d06a979933e5cc30bbb8e0905ae966c6cdf91d
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-09-29 00:47:01 +0500

    add session form to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+'use client'
+import { useEffect, useState } from "react";
 import Link from "next/link"
 
+//components
+import Dialog from "@/components/DialogForm";
+import GithubSamples from "@/components/GithubSamples"
+import { ISessionForm } from "@/shared/types";
+
 export default function Home() {
+  const [sessionForm, setSessionForm] = useState<ISessionForm>()
+
+  useEffect(() => {
+    const form = window?.sessionStorage?.getItem('form');
+    if (form) {
+      setSessionForm(JSON.parse(form))
+    }
+  }, [])
+
   return (
     <main className="h-max w-full">
-      <section className="h-full w-full px-30 py-6">
+      <section className="h-full w-full px-30 py-6 max-xl:px-20 max-lg:px-16 max-md:px-8">
         <h1 className="mb-5 !p-0">Примеры рендеринга страниц</h1>
         <ul className="flex flex-wrap items-center gap-6">
           <li className="rounded-xl bg-blue-200 px-3 py-2 text-nowrap transition hover:bg-blue-300">
@@ -19,44 +35,32 @@ export default function Home() {
             <Link href="/csr">Client Side Rendering (CSR)</Link>
           </li>
         </ul>
-        <div className="fixed bottom-20 rounded-3xl bg-neutral-100 p-10">
-          <h2 className="my-4 text-xl font-semibold">Другие примеры моих работ (github) &darr; </h2>
-          <ul className="flex flex-wrap items-center gap-5">
-            <li className="flex flex-col rounded-xl bg-neutral-200 px-4 py-2 font-medium">
-              <a
-                className="mb-1 max-w-max border-b border-b-transparent hover:border-b-current"
-                href="https://github.com/Shatlyk1011/food-delivery-app"
-                target="_blank"
-                rel="noopener"
-              >
-                Fullstack food delivery app
-              </a>
-              <span className="text-sm font-medium opacity-80">использовано: ssr, ssg, csr</span>
-            </li>
-            <li className="flex flex-col rounded-xl bg-neutral-200 px-4 py-2 font-medium">
-              <a
-                className="mb-1 max-w-max border-b border-b-transparent hover:border-b-current"
-                href="https://github.com/Edil-ozi/edil-ozi"
-                target="_blank"
-                rel="noopener"
-              >
-                Edil ozi - react component library
-              </a>
-              <span className="text-sm font-medium opacity-80">использовано: csr</span>
-            </li>
-            <li className="flex flex-col rounded-xl bg-neutral-200 px-4 py-2 font-medium">
-              <a
-                className="mb-1 max-w-max border-b border-b-transparent hover:border-b-current"
-                href="https://github.com/Shatlyk1011/blog-website"
-                target="_blank"
-                rel="noopener"
-              >
-                Personal Blog
-              </a>
-              <span className="text-sm font-medium opacity-80">использовано: ssr, ssg</span>
-            </li>
-          </ul>
+
+        <div className="py-10 flex flex-col gap-4">
+          <button className="rounded-xl text-base self-start font-medium bg-zinc-200 px-4 py-3" id="dialog_btn">Модалка с формой</button>
+          <div>
+            Данные из формы (session storage)
+
+            {sessionForm && (
+              <ul>
+                <li>Имя : {sessionForm?.name}</li>
+                <li>Email : {sessionForm?.email}</li>
+                <li>File:
+                  <div className="gap-2 inline-flex ml-2">
+
+                    {sessionForm?.files?.map((name, idx) => (
+                      <span key={idx}>{name}</span>
+                    ))}
+                  </div>
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
+
+        <GithubSamples />
+        <Dialog setSessionForm={setSessionForm} />
+
       </section>
     </main>
   )
