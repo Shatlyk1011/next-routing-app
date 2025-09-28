b7e067c8be80f9b762d6941a30442f66a347a5a6
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-09-28 22:48:58 +0500

    implement ISR

--- src/app/isr/page.tsx
+++ src/app/isr/page.tsx
@@ -1,3 +1,30 @@
+import Link from "next/link"
+import { getPosts } from "@/services/getPosts"
+
+//components
+import PostCard from "@/components/PostCard"
+import Timer from "@/components/Timer"
+
+// revalidate cache after 20 seconds
+export const revalidate = 20
+
 export default async function ISR() {
-  return <main className="h-svh w-full">ISR</main>
+  const posts = await getPosts()
+
+  return (
+    <main className="h-svh w-full">
+      <div className="px-10 py-2 relative">
+        <h1 className="!p-0 mb-2">Incremental Static Regeneration </h1>
+        <p>Аннулировать кэш через <span><Timer /></span> секунд </p>
+
+      </div>
+      <section className="grid grid-cols-4 gap-4 px-6 py-10 max-lg:grid-cols-3 max-md:grid-cols-2 ">
+        {posts.map((post) => (
+          <Link className="inline-block w-full" key={post.id} href={`/ssg/${post.id}`}>
+            <PostCard post={post} />
+          </Link>
+        ))}
+      </section>
+    </main>
+  )
 }
