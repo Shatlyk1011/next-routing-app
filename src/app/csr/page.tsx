a5b21d7ab42284a08bdb8523ca596dbeb5ed2557
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-09-28 23:27:47 +0500

    small changes

--- src/app/csr/page.tsx
+++ src/app/csr/page.tsx
@@ -19,13 +19,16 @@ export default function CSR() {
   }, [])
   return (
     <main className="h-svh w-full">
-      <h1>SSG Post Page</h1>
+      <h1>CSR Post Page</h1>
       <section className="grid grid-cols-4 gap-4 px-6 py-10 max-lg:grid-cols-3 max-md:grid-cols-2">
-        {posts.map((post) => (
+        {posts.length ? (
+          posts.map((post) => (
           <Link className="inline-block w-full" key={post.id} href={`/ssg/${post.id}`}>
             <PostCard post={post} />
           </Link>
-        ))}
+        ))) : (
+          <p>Loading...</p>
+        )}
       </section>
     </main>
   )
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -10,7 +10,7 @@ const PostCard: FC<Props> = ({ post }) => {
     <div className="h-full w-full rounded-2xl bg-blue-300 p-5">
       <span className="mb-2 inline-block text-sm font-bold">Post id {post.id}</span>
       <h2 className="mb-2 text-2xl font-normal">{post.title}</h2>
-      <p className="text-lg">{post.body}</p>
+      <p className="text-base line-clamp-3 leading-[140%]">{post.body}</p>
     </div>
   )
 }
